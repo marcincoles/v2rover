d590ff9cc98f53170df1263bd3a25d70d09c47cb
Marcin Coles <marcincoles@yahoo.com>
2008-09-03 08:12:50 +1000

    mapgen updates

--- src/mapgen/mapper.scm
+++ src/mapgen/mapper.scm
@@ -2,9 +2,26 @@
 ;mapper.scm
 ;generates maps for test purposes
 
-(provide set-vehicle-params)
+(require (planet "json.ss" ("dherman" "json.plt" 1 1)))
+
+(provide set-vehicle-params params->json)
 
 (define set-vehicle-params 
   (lambda (maxSpeed accel brake turn hardTurn rotAccel frontView rearView)
-    (display "in set-vehicle-params")
-    ))
+    ;(display "in set-vehicle-params")
+    (define p (make-hash))
+    (hash-set! p "maxSpeed" maxSpeed)
+    (hash-set! p "accel" accel)
+    (hash-set! p "brake" brake)
+    (hash-set! p "turn" turn)
+    (hash-set! p "hardTurn" hardTurn)
+    (hash-set! p "rotAccel" rotAccel)
+    (hash-set! p "frontView" frontView)
+    (hash-set! p "rearView" rearView)
+    p))
+
+(define params->json
+  (lambda (p)
+    (define o (open-output-string))
+    (write p o)
+    (get-output-string o)))
--- test/mapgen/mapper-test.scm
+++ test/mapgen/mapper-test.scm
@@ -1,14 +1,14 @@
 #lang scheme/base
-;(require (planet "main.ss" ("schematics" "schemeunit.plt" 3 3)))
 (require (planet schematics/schemeunit:3) "../../src/mapgen/mapper.scm")
-(require (planet "json.ss" ("dherman" "json.plt" 1 1)))
+;(require (planet "json.ss" ("dherman" "json.plt" 1 1)))
 
 ;test the map data generation
 
 ;firstly, check set-vehicle-params
   (test-case
    "check output of set-vehicle-params"
-   (let ( (x (set-vehicle-params 100.0 10.0 5.0 12.0 24.0 9.0 50.0 25.0)) )
+   (let ( [x (set-vehicle-params 100.0 10.0 5.0 12.0 24.0 9.0 50.0 25.0)] )
+     
      (check-pred hash? x)
      (check = (hash-ref x "maxSpeed") 100.0)
      (check = (hash-ref x "accel") 10.0)
@@ -19,8 +19,14 @@
      (check = (hash-ref x "frontView") 50.0)
      (check = (hash-ref x "rearView") 25.0)))
    
-  
-
-;function is set-map-details
+;now check that it comes out as a correctly formatted JSON message
+;when we use params->json
+  (test-case
+   "check JSON string of the hash from set-vehicle-params"
+   (let* ( [x (set-vehicle-params 100.0 10.0 5.0 12.0 24.0 9.0 50.0 25.0)] 
+          [str "{\"maxSpeed\": 100.0, \"accel\": 10.0, \"brake\": 5.0, \"turn\": 12.0, \"hardTurn\": 24.0, \"rotAccel\": 9.0, \"frontView\": 50.0, \"rearview\": 25.0}"]
+          [x-json (params->json x)])
+     (check-pred string? x-json)
+     (check string=? x-json str)))
 
 
