1cde393e7436c5bf43f736497d9669c9875e5ca9
Marcin Coles <marcincoles@yahoo.com>
2008-09-10 22:39:33 +1000

    more functions for the mapper

--- src/mapgen/mapper.scm
+++ src/mapgen/mapper.scm
@@ -1,14 +1,20 @@
-#lang scheme/base
+#lang scheme
 ;mapper.scm
 ;generates maps for test purposes
 
 (require (planet "json.ss" ("dherman" "json.plt" 1 1)))
 
-(provide set-vehicle-params params->json)
+(provide set-vehicle-params 
+	 set-martian-params 
+	 data->json 
+	 add-craters 
+	 add-boulders
+	 set-vehicle-run
+	 set-enemy-run
+	 set-enemies-run)
 
-(define set-vehicle-params 
+(define set-params 
   (lambda (maxSpeed accel brake turn hardTurn rotAccel frontView rearView)
-    ;(display "in set-vehicle-params")
     (define p (make-hash))
     (hash-set! p "maxSpeed" maxSpeed)
     (hash-set! p "accel" accel)
@@ -20,7 +26,54 @@
     (hash-set! p "rearView" rearView)
     p))
 
-(define params->json
+(define add-obstacles
+;; obstacles include craters and boulders
+  (lambda (number-of-obstacles min-rad max-rad quadrant-size)
+    (build-list number-of-obstacles 
+		(lambda (i)
+		  (define h (make-hash))
+		  (hash-set! h "x" (- (random (* quadrant-size 2)) quadrant-size))
+		  (hash-set! h "y" (- (random (* quadrant-size 2)) quadrant-size))
+		  (hash-set! h "r" (+ (* (random) (- max-rad min-rad)) min-rad))
+		  h))))
+
+(define set-vehicle-run
+;; the starting postion & direction of the rover for a run
+  (lambda (x y dir)
+    (define h (make-hash))
+    (hash-set! h "x" x)
+    (hash-set! h "y" y)
+    (hash-set! h "dir" dir)
+    h))
+
+(define set-enemy-run
+;; the starting postion & direction of a martian for a run
+  (lambda (x y dir speed view)
+    (define h (set-vehicle-run x y dir))
+    (hash-set! h "speed" speed)
+    (hash-set! h "view" view)
+    h))
+
+(define set-enemies-run
+  (lambda (number-of-martians quadrant-size max-speed max-view)
+    (build-list number-of-martians
+		(lambda (i)
+		  (set-enemy-run (- (random (* quadrant-size 2)) quadrant-size)
+				 (- (random (* quadrant-size 2)) quadrant-size)
+				 (* 360 (random))
+				 (* max-speed (random))
+				 (* max-view (random)))))))
+
+
+(define set-vehicle-params set-params)
+
+(define set-martian-params set-params)
+
+(define add-craters add-obstacles)
+
+(define add-boulders add-obstacles)
+
+(define data->json
   (lambda (p)
     (define o (open-output-string))
     (write p o)
